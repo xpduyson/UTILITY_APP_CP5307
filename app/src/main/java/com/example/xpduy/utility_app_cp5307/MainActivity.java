ae39376276a06f3901d5ba86906c69ce7348949b
Dang Duy Son <xpduyson@gmail.com>
2018-03-17 19:16:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xpduy/utility_app_cp5307/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xpduy.utility_app_cp5307;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
